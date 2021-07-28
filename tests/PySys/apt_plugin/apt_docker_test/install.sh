cfa341b48c5c8a37354e886f26e60c3337dddfc7
Pradeep Kumar K J <pradeepkumar.kj@sofwareag.com>
2021-07-28 18:49:53 +0530

    add multiple more tests

--- tests/PySys/apt_plugin/apt_docker_test/install.sh
+++ tests/PySys/apt_plugin/apt_docker_test/install.sh
@@ -1,3 +1,3 @@
 dpkg -i /debian/*.deb; apt-get install -y -f
-cd /tests/PySys; pysys.py run apt_remove
+cd /tests/PySys; pysys.py run apt_remove apt_sequence_install_with_version apt_sequence_remove_with_version
 exit
