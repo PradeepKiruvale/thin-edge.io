8d2dec680804c3527c7afa581d3476b14752b272
Pradeep Kumar K J <pkj@softwareag.com>
2023-07-07 06:47:44 +0530

    use default for response

--- crates/core/c8y_api/src/json_c8y.rs
+++ crates/core/c8y_api/src/json_c8y.rs
@@ -98,7 +98,7 @@ impl From<SoftwareModule> for C8ySoftwareModuleItem {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct C8yUpdateSoftwareListResponse {
     #[serde(rename = "c8y_SoftwareList")]
@@ -117,14 +117,6 @@ impl From<&SoftwareListResponse> for C8yUpdateSoftwareListResponse {
 
         Self {
             c8y_software_list: Some(new_list),
-        }
-    }
-}
-
-impl Default for C8yUpdateSoftwareListResponse {
-    fn default() -> Self {
-        Self {
-            c8y_software_list: None,
         }
     }
 }
